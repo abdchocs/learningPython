36f12103e57c4fe3a4fdac9480b6933e315544bf
Abdi Nugroho <144451432+abdchocs@users.noreply.github.com>
2023-10-24 09:17:04 +0700

    Add files via upload

---
+++ pemilihan2.py
@@ -0,0 +1,7 @@
+nilaiA = int(input("MAsukkan nilai A = "))
+nilaiB = int(input("MAsukkan nilai B = "))
+
+if (nilaiA>nilaiB) :
+    print("Bilangan Terbesar adalah nilai A")
+else :
+    print("Bilangan Terbesar adalah nilai B")
