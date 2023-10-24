df89c4321b4f54a412acb9e34489a033e1363ced
Abdi Nugroho <144451432+abdchocs@users.noreply.github.com>
2023-10-24 09:17:17 +0700

    Add files via upload

---
+++ pemilihan3.py
@@ -0,0 +1,11 @@
+nilai = int(input("inputkan nilai = "))
+
+
+if(nilai >= 60)and(nilai <= 69) : 
+    print("indeks = C")
+elif (nilai >= 70)and(nilai <= 79) :
+    print("indeks = B")
+elif (nilai >= 80)and(nilai <= 100) :
+    print("indeks = A")
+else :
+    print("nilai tidak di definisikan")
