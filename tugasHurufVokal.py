7650d81b3a304ad2ec9fcf4a123883442e6cfdaa
Abdi Nugroho <144451432+abdchocs@users.noreply.github.com>
2023-11-24 07:21:51 +0700

    Update tugasHurufVokal.py

--- tugasHurufVokal.py
+++ tugasHurufVokal.py
@@ -1,5 +1,5 @@
 c = input("Masukkan nilai = ")
 
 
-if (c=='a')or (c=='i') or (c=='u') or (c=='e') or (c=='0'):
+if (c=='a')or (c=='i') or (c=='u') or (c=='e') or (c=='o'):
     print("huruf vokal")
